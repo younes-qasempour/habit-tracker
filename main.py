d0361364e712a23ce0b2284a535c58a077abb5ec
younes_qasempour <qasempouryounes@gmail.com>
2025-02-27 19:33:13 +0330

    Initial commit

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
 from data import *
 
-pixela_endpoint = "https://pixe.la/v1/users "
+pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
     'token': MY_TOKEN,
-
+    'username': 'qasempouryounes',
+    'agreeTermsOfService': 'yes',
+    'notMinor': 'yes',
 }
-requests.post(pixela_endpoint)
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
